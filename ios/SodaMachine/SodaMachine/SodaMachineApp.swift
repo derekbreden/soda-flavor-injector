4bd3f0ac88605c1034942e5abcf9564389a5362f
Derek Bredensteiner <derekbreden@gmail.com>
2026-04-03 02:05:11 -0500

    Revert "Fix BLE stalls after long background periods on iOS"
    This reverts commit 029d9589194941ec6738c3faa7b0e8a73f30fc2b.

--- ios/SodaMachine/SodaMachine/SodaMachineApp.swift
+++ ios/SodaMachine/SodaMachine/SodaMachineApp.swift
@@ -3,17 +3,11 @@ import SwiftUI
 @main
 struct SodaMachineApp: App {
     @State private var bleManager = BLEManager()
-    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ScanView()
                 .environment(bleManager)
-                .onChange(of: scenePhase) { _, phase in
-                    if phase == .active {
-                        bleManager.handleReturnToForeground()
-                    }
-                }
         }
     }
 }
